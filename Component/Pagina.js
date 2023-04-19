026994ffe5887c33707b7043f3721a63301fde90
Spoknimoy <luanpablo465@gmail.com>
2023-04-19 17:43:17 -0300

    atualização1

--- Component/Pagina.js
+++ Component/Pagina.js
@@ -4,33 +4,41 @@ import Cabecalho from './Cabecalho'
 import Rodape from './Rodape'
 import { Container } from 'react-bootstrap';
 
-const Pagina = (props) => {
+const Pagina = () => {
 
     const estiloBackground = {
-        background: 'linear-gradient(to bottom, #FFFFFF, #ECECEC)',
-        height: '100vh'
+        background: 'rgba(14, 40, 237, 0.96)',
+        height: '40vh',
+        width: '100%'
     }
 
     const estiloTexto = {
-        color: '#333',
+        color: 'rgba(255, 255, 255, 0.8)',
         fontSize: '2rem',
         fontWeight: 'bold',
-        textAlign: 'center',
-        paddingTop: '20vh'
+        
+        paddingTop: '9vh'
     }
 
     return (
         <>
             <Cabecalho />
-            <Container style={estiloBackground}>
-                <div style={estiloTexto}>
-                    <h1>{props.titulo}</h1>
-                    <p>Este é um exemplo de texto dentro de um background.</p>
+            <div style={estiloBackground}>
+                <div>
+                    <h1 style={estiloTexto}>Dados abertos</h1>
+                    <p>COlá! Se você estiver procurando por dados selecionados em pequenas quantidades, eu recomendaria experimentar a nova
+                        API RESTful disponível aqui. Esta API fornece URLs para acessar os dados, parâmetros de busca e estruturas de dados
+                        retornadas.
+
+                        Por outro lado, se você deseja baixar conjuntos completos e extensos de dados de uma única vez para serem selecionados e processados em
+                        seu computador, sugiro que acesse a seção de Arquivos. Lá, você poderá escolher o formato mais adequado para sua necessidade.
+
+                        Divirta-se com os dados! Se você tiver alguma dúvida ou precisar de ajuda adicional, não hesite em me perguntar.</p>
                 </div>
-            </Container>
+            </div>
+
             <Rodape />
         </>
     )
 }
-
 export default Pagina
