be8eaf5677680ee2d4d4647daa85ca3ce9a13508
Spoknimoy <luanpablo465@gmail.com>
2023-04-20 01:03:10 -0300

    fiz o backgroud azul e loyout dos componentes

--- Component/Pagina.js
+++ Component/Pagina.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Cabecalho from './Cabecalho'
 import Rodape from './Rodape'
 import { Container } from 'react-bootstrap';
+import logo from '../pages/img/mulher22.png';
 
 const Pagina = () => {
 
@@ -12,33 +13,54 @@ const Pagina = () => {
         width: '100%'
     }
 
-    const estiloTexto = {
+
+    const estilotitulo = {
         color: 'rgba(255, 255, 255, 0.8)',
-        fontSize: '2rem',
+        fontSize: '3rem', // alterado de 2rem para 3rem
         fontWeight: 'bold',
-        
-        paddingTop: '9vh'
+        paddingRight: '15vh',
+        marginTop: '2rem' // adicionado margem para cima
+    }
+
+    const estilotexto = {
+        color: 'rgba(255, 255, 255, 0.8)',
+        paddingRight: '15vh',
+        fontSize: '1rem'
+    }
+
+    const mulher22 = {
+        width: '50vh',
+        height: '48.3vh',
+        loading: "lazy",
+        margintop: '23px'
+    }
+
+    const texto = {
+        marginBottom: '8vh'
     }
 
     return (
         <>
             <Cabecalho />
             <div style={estiloBackground}>
-                <div>
-                    <h1 style={estiloTexto}>Dados abertos</h1>
-                    <p>COlá! Se você estiver procurando por dados selecionados em pequenas quantidades, eu recomendaria experimentar a nova
-                        API RESTful disponível aqui. Esta API fornece URLs para acessar os dados, parâmetros de busca e estruturas de dados
-                        retornadas.
+                <Container className="d-flex flex-row-reverse align-items-center">
+                    <img style={mulher22} src='https://previews-tu.wetransfer.net/file/wetransfer/p1ot/3453d05ab857e5a9da31798f5298849320230419200415/hl-9020748185?width=512&height=512&source=storm&origin=transfer&url=https%3A%2F%2Fstorm-us-east-1.wetransfer.net%2Ffiles%2FeyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHNLd2dzSU0wWUFnQT0iLCJleHAiOm51bGwsInB1ciI6ImludGVybmFsX2ZpbGVfZG93bmxvYWQifX0--6cb7fc54f88dfc6dd472200aa38265a76e6ea76142ebffe3fa420f7ccccc9185%3Ftoken%3DeyJhbGciOiJIUzI1NiJ9.eyJzdG9ybS5zZmUiOiJleUpmY21GcGJITWlPbnNpYldWemMyRm5aU0k2SWtKQmFITkxkMmR6U1Uwd1dVRm5RVDBpTENKbGVIQWlPbTUxYkd3c0luQjFjaUk2SW1sdWRHVnlibUZzWDJacGJHVmZaRzkzYm14dllXUWlmWDAtLTZjYjdmYzU0Zjg4ZGZjNmRkNDcyMjAwYWEzODI2NWE3NmU2ZWE3NjE0MmViZmZlM2ZhNDIwZjdjY2NjYzkxODUiLCJleHAiOjE2ODE5NjIwMzYsImlhdCI6MTY4MTk1ODQzNiwia2lkIjoiRnIzciJ9.jz-nERDv1x_CfxzTfAL_Nbs-svhjJ7Smb0sWGFHf7eQ&s=f4909b393cfa3599c3a5a3ae44438ff240e12fc9&Expires=1681962036&Signature=GoDC8zpHPdgL6jkvtAPCD2CL51o-7aRS2k5xnEclyX0W3gzErigbv3bfsfjdZmMlXEvXr59PgWNzBaSJwukmlpgB050rqPNlsXQKdrF2P9zLZ6V45hXv0LoTX7Uyx8CgPk9UATdTDy999RoiIqNCDqmBXP6Q9I90J9JFH~pCMXltUpUERU6xF8VzNmbk2wTPn-px9Fu-92gzIRdEwGY9GH~iTX7-3K4wRC9EQAi03hImBzRyHNO2OT8-EAenRiDEoSWwnjW3r-JZAsqo3KvMRZPPdfgTnjXo62kVYDnkifteDLLUuCbtWnuzrL6VVGEjl6YhF0mGELF9Jhp9JEpd2Q__&Key-Pair-Id=APKAIRLQFERKGUWFG7GQ' alt="Descrição da imagem" />
+                    <div className="" style={texto}>
+                        <h1 style={estilotitulo}>Dados abertos</h1>
+                        <p style={estilotexto}>Se você precisa de dados selecionados, um pouco de cada vez, conheça e experimente aqui mesmo a nova API RESTful, suas URLs para acesso aos dados, seus parâmetros de busca e as estruturas de dados retornadas.</p>
 
-                        Por outro lado, se você deseja baixar conjuntos completos e extensos de dados de uma única vez para serem selecionados e processados em
-                        seu computador, sugiro que acesse a seção de Arquivos. Lá, você poderá escolher o formato mais adequado para sua necessidade.
+                           <p style={estilotexto}> Se você prefere fazer download de conjuntos completos (e grandes) de dados de uma só vez, para selecionar e processar no seu computador, visite a seção de Arquivos e escolha o formato mais apropriado para você.
 
-                        Divirta-se com os dados! Se você tiver alguma dúvida ou precisar de ajuda adicional, não hesite em me perguntar.</p>
-                </div>
+
+                        </p>
+
+                        <p style={estilotexto}><strong>Bom trabalho e divirta-se!</strong></p>
+                    </div>
+                </Container>
             </div>
-
             <Rodape />
         </>
     )
 }
-export default Pagina
+
+export default Pagina;
