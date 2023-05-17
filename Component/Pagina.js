acc7506107f9f37fa1c40d09403dfef8f1da31f0
Spoknimoy <luanpablo465@gmail.com>
2023-05-17 20:38:16 -0300

    ATUALIZAÇÃO DA PIMEIRA PAGINA

--- Component/Pagina.js
+++ Component/Pagina.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Cabecalho from './Cabecalho'
 import Rodape from './Rodape'
 import { Container } from 'react-bootstrap';
-import logo from '../pages/img/mulher22.png';
+import logs from './imagens/m.png';
 
 const Pagina = () => {
 
@@ -44,7 +44,7 @@ const Pagina = () => {
             <Cabecalho />
             <div style={estiloBackground}>
                 <Container className="d-flex flex-row-reverse align-items-center">
-                    <img style={mulher22} src='https://previews-tu.wetransfer.net/file/wetransfer/p1ot/3453d05ab857e5a9da31798f5298849320230419200415/hl-9020748185?width=512&height=512&source=storm&origin=transfer&url=https%3A%2F%2Fstorm-us-east-1.wetransfer.net%2Ffiles%2FeyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHNLd2dzSU0wWUFnQT0iLCJleHAiOm51bGwsInB1ciI6ImludGVybmFsX2ZpbGVfZG93bmxvYWQifX0--6cb7fc54f88dfc6dd472200aa38265a76e6ea76142ebffe3fa420f7ccccc9185%3Ftoken%3DeyJhbGciOiJIUzI1NiJ9.eyJzdG9ybS5zZmUiOiJleUpmY21GcGJITWlPbnNpYldWemMyRm5aU0k2SWtKQmFITkxkMmR6U1Uwd1dVRm5RVDBpTENKbGVIQWlPbTUxYkd3c0luQjFjaUk2SW1sdWRHVnlibUZzWDJacGJHVmZaRzkzYm14dllXUWlmWDAtLTZjYjdmYzU0Zjg4ZGZjNmRkNDcyMjAwYWEzODI2NWE3NmU2ZWE3NjE0MmViZmZlM2ZhNDIwZjdjY2NjYzkxODUiLCJleHAiOjE2ODE5NjIwMzYsImlhdCI6MTY4MTk1ODQzNiwia2lkIjoiRnIzciJ9.jz-nERDv1x_CfxzTfAL_Nbs-svhjJ7Smb0sWGFHf7eQ&s=f4909b393cfa3599c3a5a3ae44438ff240e12fc9&Expires=1681962036&Signature=GoDC8zpHPdgL6jkvtAPCD2CL51o-7aRS2k5xnEclyX0W3gzErigbv3bfsfjdZmMlXEvXr59PgWNzBaSJwukmlpgB050rqPNlsXQKdrF2P9zLZ6V45hXv0LoTX7Uyx8CgPk9UATdTDy999RoiIqNCDqmBXP6Q9I90J9JFH~pCMXltUpUERU6xF8VzNmbk2wTPn-px9Fu-92gzIRdEwGY9GH~iTX7-3K4wRC9EQAi03hImBzRyHNO2OT8-EAenRiDEoSWwnjW3r-JZAsqo3KvMRZPPdfgTnjXo62kVYDnkifteDLLUuCbtWnuzrL6VVGEjl6YhF0mGELF9Jhp9JEpd2Q__&Key-Pair-Id=APKAIRLQFERKGUWFG7GQ' alt="Descrição da imagem" />
+                    <img style={mulher22} src={logs} alt="Descrição da imagem"  title='Imagem da api'/>
                     <div className="" style={texto}>
                         <h1 style={estilotitulo}>Dados abertos</h1>
                         <p style={estilotexto}>Se você precisa de dados selecionados, um pouco de cada vez, conheça e experimente aqui mesmo a nova API RESTful, suas URLs para acesso aos dados, seus parâmetros de busca e as estruturas de dados retornadas.</p>
--- pages/deputados/index.js
+++ pages/deputados/index.js
@@ -1,10 +1,43 @@
 import Pagina from '@/Component/Pagina'
+import Link from 'next/link'
 import React from 'react'
+import { Container, Nav, Navbar } from 'react-bootstrap'
+
+import logo from '../../imagens/m.png'
 
 const index = () => {
   return (
     <>
-    <Pagina></Pagina>
+      <Pagina />
+
+
+      <Navbar className='mt-4' bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">API RESTful</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#pricing">ARQUIVOS</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+       <Container>
+
+      <p className='mt-1'><strong>Versão: 0.4.80 - 05/10/2023 04:52 PM </strong>- confira o que mudou na <Link href='https://dadosabertos.camara.leg.br/news/noticias.html'>página de Notícias</Link></p>
+
+      <p>Bem vindo à nova versão do serviço <strong> Dados Abertos</strong> da Câmara dos Deputados!</p>
+      <p>Esta versão pode entregar dados puros em formatos JSON e XML, e tenta se aderir ao máximo à arquitetura REST</p>
+      <p>Nesta página você pode conhecer e experimentar as URLs de acesso aos dados, os parâmetros de query string que podem ser aplicados para filtrar e selecionar resultados, e as estruturas de dados que são retornadas.</p>
+      <p>Além do método HTTP GET, você também pode usar o método HEAD com qualquer dos serviços.</p>
+      <p>Por padrão, todos os serviços de listagens retornam 15 itens, e o limite por requisição é de 100 itens.</p>
+      <p><strong>ATENÇÃO: </strong>Esta versão é ainda incompleta, sujeita a mudanças e não substitui a <Link href='https://www2.camara.leg.br/transparencia/dados-abertos/dados-abertos-legislativo'>versão original do <strong> Dados Abertos</strong></Link>. Caso você encontre problemas ou queira dar sugestões, por favor entre em <Link href='https://dadosabertos.camara.leg.br/contact/contact.html'>contato.</Link></p>
+
+       </Container>
+
+       
+
+       
+
+
+
     </>
   )
 }
